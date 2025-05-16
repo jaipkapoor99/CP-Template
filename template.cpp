2c92a71a8258691e4001adf8e1f90806ed99efa5
Jai Kapoor <jaipkapoor99@gmail.com>
2025-05-17 04:42:28 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,198 +1,242 @@
-#include <bits/stdc++.h>
-using namespace std;
+// C++20 CP Template
 
-// -----------------------------------------------------------------------------
+#include <bits/stdc++.h>
+#include <bits/extc++.h>
+#include <ranges>
+#include <bit>
+#include <numbers>
+#include <span>
+#include <chrono>
+#include <random>
+#include <source_location>
+using namespace std;
+using namespace __gnu_pbds;
+
+// ------------------------------------
+// Type Aliases
+// ------------------------------------
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vll = vector<ll>;
+using vb = vector<bool>;
+using vpii = vector<pii>;
+using vpll = vector<pll>;
+using vs = vector<string>;
+using vd = vector<double>;
+using vld = vector<long double>;
+using dq = deque<int>;
+template<typename T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+template<typename T> using pql = priority_queue<T>;
+template<typename T> using os = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template<typename K, typename V> using omap = tree<K, V, less<K>, rb_tree_tag, tree_order_statistics_node_update>;
+template<typename T> using uset = unordered_set<T>;
+template<typename K, typename V> using umap = unordered_map<K, V>;
+
+// ------------------------------------
+// Constants
+// ------------------------------------
+constexpr int MOD = 1'000'000'007;
+constexpr int MOD1 = 998'244'353;
+constexpr char SPACE = ' ';
+constexpr char NL = '\n';
+constexpr double EPS = 1e-9;
+constexpr double PI = std::numbers::pi;
+constexpr ll INF = 1e18;
+constexpr int INF32 = 1e9;
+
+// ------------------------------------
 // Fast Input/Output
-// -----------------------------------------------------------------------------
+// ------------------------------------
 #define FASTINOUT ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr)
 
-// -----------------------------------------------------------------------------
-// Type Aliases
-// -----------------------------------------------------------------------------
-using ll    = long long;
-using ld    = long double;
-using pii   = pair<int, int>;
-using pll   = pair<ll, ll>;
-using tll   = tuple<ll, ll, ll>;
-using vi    = vector<int>;
-using vll   = vector<ll>;
-using vb    = vector<bool>;
-using vvb   = vector<vb>;
-using vpii  = vector<pii>;
-using vpll  = vector<pll>;
-using vtll  = vector<tll>;
-using vvll  = vector<vll>;
-using vvpii = vector<vpii>;
-using vvpll = vector<vpll>;
-using mii   = map<int, int>;
-using mll   = map<ll, ll>;
-using si    = set<int>;
-using sll   = set<ll>;
-using sc    = set<char>;
+// ------------------------------------
+// Macros
+// ------------------------------------
+#define f(i, s, e)      for (ll i = (s); i < (e); ++i)
+#define cf(i, s, e)     for (ll i = (s); i <= (e); ++i)
+#define rf(i, e, s)     for (ll i = (e) - 1; i >= (s); --i)
+#define all(v)          (v).begin(), (v).end()
+#define rall(v)         (v).rbegin(), (v).rend()
+#define sz(v)           ((ll)(v).size())
+#define pb              push_back
+#define eb              emplace_back
+#define fi              first
+#define se              second
+#define YES             cout << "YES" << NL
+#define NO              cout << "NO" << NL
+#define endl            NL
+#define UNIQUE(v)       sort(all(v)), v.erase(unique(all(v)), v.end())
+#define forV(v)         for (auto& e : (v))
+#define forM(m)         for (auto& [key, value] : (m))
 
-// -----------------------------------------------------------------------------
-// Constants
-// -----------------------------------------------------------------------------
-constexpr int  MOD  = 1'000'000'007;
-constexpr int  MOD1 = 998'244'353;
-constexpr char SPACE = ' ';
-constexpr char NL    = '\n';
+// C++20 Ranges helpers
+namespace ranges = std::ranges;
+namespace views = std::views;
+#define FORR(v)         for (auto&& e : (v | views::reverse))
+#define TRANS(v, op)    for (auto&& e : (v | views::transform(op)))
 
-// -----------------------------------------------------------------------------
-// Loop Macros
-// -----------------------------------------------------------------------------
-#define f(i, s, e)    for (ll i = (s); i < (e); ++i)
-#define cf(i, s, e)   for (ll i = (s); i <= (e); ++i)
-#define rf(i, e, s)   for (ll i = (e) - 1; i >= (s); --i)
-#define all(v)        (v).begin(), (v).end()
-#define rall(v)       (v).rbegin(), (v).rend()
-#define forV(v)       for (auto& e : (v))
-#define forM(m)       for (auto& [key, value] : (m))
-#define newline       cout << NL
-#define space         SPACE
+// ------------------------------------
+// Policy-based Data Structures (PBDS)
+// ------------------------------------
+// Usage: os<int> s; s.insert(5); s.order_of_key(5);
+// *s.find_by_order(0);
 
-// -----------------------------------------------------------------------------
-// Debugging Helpers (prints to stderr if LOCAL is defined)
-// -----------------------------------------------------------------------------
+// ------------------------------------
+// Random Generator
+// ------------------------------------
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+ll rand_ll(ll l, ll r) { return uniform_int_distribution<ll>(l, r)(rng); }
+
+// ------------------------------------
+// Custom Hash for Unordered Containers
+// ------------------------------------
+struct custom_hash {
+    static ull splitmix64(ull x) {
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+    size_t operator()(ull x) const {
+        static const ull FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
+template<typename T>
+using safe_unordered_set = unordered_set<T, custom_hash>;
+template<typename K, typename V>
+using safe_unordered_map = unordered_map<K, V, custom_hash>;
+
+// ------------------------------------
+// Bit Manipulation (C++20)
+// ------------------------------------
+template<typename T> constexpr bool is_pow2(T x) { return std::has_single_bit(x); }
+template<typename T> constexpr int popcnt(T x) { return std::popcount(x); }
+template<typename T> constexpr int lsb(T x) { return std::countr_zero(x); }
+template<typename T> constexpr int msb(T x) { return std::bit_width(x) - 1; }
+
+// ------------------------------------
+// Debugging (source_location, colorized, powerful)
+// ------------------------------------
 #ifdef LOCAL
-#define dbg(...) cerr << "[" << #__VA_ARGS__ << "]: ", debug_out(__VA_ARGS__)
-template<typename T>
-void __print(const T &x) { cerr << x; }
-template<typename T, typename U>
-void __print(const pair<T, U> &x) { cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}'; }
-template<typename T>
-void __print(const vector<T> &x) { cerr << '{'; for (auto &i : x) { __print(i); cerr << ' '; } cerr << '}'; }
-template<typename T>
-void __print(const set<T> &x) { cerr << '{'; for (auto &i : x) { __print(i); cerr << ' '; } cerr << '}'; }
-template<typename T, typename U>
-void __print(const map<T, U> &x) { cerr << '{'; for (auto &i : x) { __print(i); cerr << ' '; } cerr << '}'; }
+#define RESET   "\033[0m"
+#define RED     "\033[31m"
+#define GREEN   "\033[32m"
+#define YELLOW  "\033[33m"
+#define BLUE    "\033[34m"
 template<typename... Args>
-void debug_out(Args... args) { int n = 0; (void)initializer_list<int>{(__print(args), cerr << (++n == sizeof...(args) ? '\n' : " | "), 0)...}; }
+void dbg(const std::source_location loc = std::source_location::current(), Args&&... args) {
+    cerr << BLUE << "[" << loc.file_name() << ":" << loc.line() << " " << loc.function_name() << "] " << RESET;
+    ((cerr << RED << args << RESET << " | "), ...);
+    cerr << '\n';
+}
+#define DEBUG(...) dbg(std::source_location::current(), __VA_ARGS__)
 #else
-#define dbg(...)
+#define DEBUG(...)
 #endif
 
-// -----------------------------------------------------------------------------
-// Input Helpers (overloaded 'read')
-// -----------------------------------------------------------------------------
+// ------------------------------------
+// Timer Utility
+// ------------------------------------
+class Timer {
+    chrono::high_resolution_clock::time_point st;
+public:
+    Timer() : st(chrono::high_resolution_clock::now()) {}
+    void reset() { st = chrono::high_resolution_clock::now(); }
+    ll elapsed() const {
+        return chrono::duration_cast<chrono::milliseconds>(chrono::high_resolution_clock::now() - st).count();
+    }
+};
+
+// ------------------------------------
+// Input/Output Helpers (C++20 span/tuple, variadic)
+// ------------------------------------
+template<typename... Ts>
+istream& operator>>(istream& is, tuple<Ts...>& t) {
+    apply([&is](auto&... args){ ((is >> args), ...); }, t);
+    return is;
+}
+template<typename... Ts>
+ostream& operator<<(ostream& os, const tuple<Ts...>& t) {
+    apply([&os](const auto&... args){ size_t n = 0; ((os << (n++ ? SPACE : "") << args), ...); }, t);
+    return os;
+}
+template<typename T>
+istream& operator>>(istream& is, span<T> v) { for (auto& x : v) is >> x; return is; }
+template<typename T>
+ostream& operator<<(ostream& os, span<const T> v) { for (size_t i = 0; i < v.size(); ++i) os << (i ? SPACE : "") << v[i]; return os; }
+
+// ------------------------------------
+// Modular Arithmetic (class for reusability)
+// ------------------------------------
+template<int MOD>
+struct Mint {
+    int v;
+    Mint(ll _v = 0) : v(norm(_v)) {}
+    static int norm(ll x) { x %= MOD; if (x < 0) x += MOD; return x; }
+    Mint operator + (const Mint& b) const { return v + b.v; }
+    Mint operator - (const Mint& b) const { return v - b.v + MOD; }
+    Mint operator * (const Mint& b) const { return 1LL * v * b.v; }
+    Mint pow(ll p) const {
+        Mint a = *this, r = 1;
+        while (p) { if (p & 1) r = r * a; a = a * a; p >>= 1; }
+        return r;
+    }
+    Mint inv() const { return pow(MOD - 2); }
+    Mint operator / (const Mint& b) const { return *this * b.inv(); }
+    Mint& operator+=(const Mint& b) { v = norm(v + b.v); return *this; }
+    Mint& operator-=(const Mint& b) { v = norm(v - b.v); return *this; }
+    Mint& operator*=(const Mint& b) { v = norm(1LL * v * b.v); return *this; }
+    Mint& operator/=(const Mint& b) { return *this *= b.inv(); }
+    friend ostream& operator<<(ostream& os, const Mint& m) { return os << m.v; }
+    friend istream& operator>>(istream& is, Mint& m) { ll x; is >> x; m = Mint(x); return is; }
+};
+
+// ------------------------------------
+// Read/Print Helpers (Variadic)
+// ------------------------------------
 template<typename T>
 void read(T& x) { cin >> x; }
-template<typename T, typename U>
-void read(pair<T, U>& p) { cin >> p.first >> p.second; }
+template<typename T, typename... Args>
+void read(T& x, Args&... args) { read(x); read(args...); }
 template<typename T>
-void read(vector<T>& v) { for (auto& x : v) read(x); }
-template<typename T, typename U>
-void read(vector<pair<T, U>>& v) { for (auto& x : v) read(x); }
+void print(const T& x) { cout << x << NL; }
+template<typename T, typename... Args>
+void print(const T& x, const Args&... args) { cout << x << SPACE; print(args...); }
+template<typename T>
+void printv(const vector<T>& v) { for (auto &x : v) cout << x << SPACE; cout << NL; }
 
-// -----------------------------------------------------------------------------
-// Output Helpers (overloaded 'print')
-// -----------------------------------------------------------------------------
+// ------------------------------------
+// Math Utilities (add your own as needed)
+// ------------------------------------
 template<typename T>
-void print(const T& x, char endc = NL) { cout << x << endc; }
-template<typename T, typename U>
-void print(const pair<T, U>& p, char endc = NL) { cout << p.first << SPACE << p.second << endc; }
+T gcd(T a, T b) { return b ? gcd(b, a % b) : a; }
 template<typename T>
-void print(const vector<T>& v, char sep = SPACE, char endc = NL) {
-    for (size_t i = 0; i < v.size(); ++i) {
-        if (i) cout << sep;
-        cout << v[i];
-    }
-    cout << endc;
-}
-template<typename T, typename U>
-void print(const vector<pair<T, U>>& v, char sep = SPACE, char endc = NL) {
-    for (size_t i = 0; i < v.size(); ++i) {
-        if (i) cout << sep;
-        print(v[i], '\0');
-    }
-    cout << endc;
-}
+T lcm(T a, T b) { return a / gcd(a, b) * b; }
+template<typename T>
+T ceildiv(T a, T b) { return (a + b - 1) / b; }
+template<typename T>
+bool is_prime(T n) { if (n <= 1) return false; for (T i = 2; i * i <= n; ++i) if (n % i == 0) return false; return true; }
+// ... (add more as you need)
 
-// Convenience
-inline void yes() { cout << "YES" << NL; }
-inline void no()  { cout << "NO" << NL; }
-
-// -----------------------------------------------------------------------------
-// Math and Utilities
-// -----------------------------------------------------------------------------
-template<typename T>
-T getMod(T a, T m = MOD) { T ans = a % m; return (ans < 0) ? ans + m : ans; }
-template<typename T>
-bool odd(T n) { return n & 1; }
-template<typename T>
-bool isDivisible(T n, T i) { return getMod(n, i) == 0; }
-template<typename T>
-T modMul(T a, T b, T mod = MOD) { return getMod(a * b, mod); }
-template<typename T>
-T modAdd(T a, T b, T mod = MOD) { return getMod(a + b, mod); }
-template<typename T>
-T modSub(T a, T b, T mod = MOD) { return getMod(a - b + mod, mod); }
-template<typename T>
-T ceilDiv(T a, T b) { return (a + b - 1) / b; }
-template<typename T>
-void toMax(T &a, T b) { a = max(a, b); }
-template<typename T>
-void toMin(T &a, T b) { a = min(a, b); }
-template<typename T>
-bool coPrime(T a, T b) { return gcd(a, b) == 1; }
-template<typename T>
-T gcdExtended(T a, T b, T &x, T &y) {
-    if (!a) { x = 0, y = 1; return b; }
-    T x1, y1;
-    T g = gcdExtended(getMod(b, a), a, x1, y1);
-    x = y1 - (b / a) * x1, y = x1;
-    return g;
-}
-template<typename T>
-T modInverse(T b, T m = MOD) {
-    T x, y;
-    T g = gcdExtended(b, m, x, y);
-    return getMod(getMod(x, m) + m, m);
-}
-template<typename T>
-T modDivide(T a, T b, T m = MOD) { return getMod(modInverse(b) * getMod(a, m), m); }
-template<typename T>
-T modPow(T a, T b, T m = MOD) {
-    T res = 1; a %= m;
-    while (b) {
-        if (b & 1) res = (res * a) % m;
-        a = (a * a) % m; b >>= 1;
-    }
-    return res;
-}
-template<typename T>
-bool prime(T a) {
-    if (a == 1) return false;
-    cf(i, 2, sqrtl(a)) if (isDivisible(a, i)) return false;
-    return true;
-}
-template<typename T>
-bool perfectSquare(T n) { return ceil((long double)sqrtl(n)) == floor((long double)sqrtl(n)); }
-template<typename T>
-T sumOfNNaturalNumbers(T n) { return n * (n + 1) / 2ll; }
-template<typename T>
-T lcm(T a, T b) { return (a * b) / gcd(a, b); }
-template<typename T, typename U>
-vector<pair<T, U>> mapToVecPair(const map<T, U>& m) {
-    vector<pair<T, U>> v;
-    for (auto& e : m) v.push_back(e);
-    return v;
-}
-
-// -----------------------------------------------------------------------------
-// Problem Solver
-// -----------------------------------------------------------------------------
+// ------------------------------------
+// Main Solve Function
+// ------------------------------------
 void solve() {
     // Example usage:
     // int n; read(n);
     // vi a(n); read(a);
-    // dbg(n, a); // for local debugging
-    // print(a);
+    // DEBUG(n, a);
+    // printv(a);
 }
 
-// -----------------------------------------------------------------------------
+// ------------------------------------
 // Main
-// -----------------------------------------------------------------------------
+// ------------------------------------
 int main() {
     FASTINOUT;
     int t = 1;
